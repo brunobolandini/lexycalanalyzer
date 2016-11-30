58e9b92badba98f68c7ec3cd8c3df71310220588
brunobolandini <bruno.svp@gmail.com>
2016-11-30 11:21:09 -0200

    first commit

--- src/main/Lua.java
+++ src/main/Lua.java
@@ -6,7 +6,7 @@ import java.io.StringReader;
 public class Lua {
 	public static void main(String[] args) throws IOException {
 
-        String expr = "if 2 + 3+a then";
+        String expr = "if 2 + 3 + a then";
 
         LexicalAnalyzer lexical = new LexicalAnalyzer(new StringReader(expr));
         lexical.yylex();
