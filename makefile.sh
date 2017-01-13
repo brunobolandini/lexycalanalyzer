9424a0a50ba4ebbed6b3c7685d08917d21221ba8
Raissa Amaral <raissa.amaral@visagio.com.br>
2017-01-13 17:15:50 -0200

    adicionando remocao de arquivos gerados automaticamente ao makefile

--- makefile.sh
+++ makefile.sh
@@ -3,12 +3,16 @@
 # Navegar para pasta lua:
 cd src/lua
 
+# Removendo arquivos gerados em outra execução
+rm -f src/lua/Lexer.java
+rm -f src/lua/Parser.java
+rm -f src/lua/Sym.java
 
 # Para criação do Lexer.java:
 java -jar ../../jflex-1.6.1.jar Lexer.lex 
 
 # Para criação do Parser.java e Sym.java:
-java -jar ../../java-cup-11a.jar -parser Parser -symbols Sym Parser.cup
+java -jar ../../java-cup-11a.jar -parser Parser -expect 11 -symbols Sym Parser.cup
 
 
 # EXECUÇÃO POR LINHA DE COMANDO:
